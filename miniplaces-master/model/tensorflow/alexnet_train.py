0bf417ead94558d6f2651558c6f240b1d8f82a06
Chris Larry <larry@mit.edu>
2017-11-16 09:43:44 -0500

    fixed layer widths

--- miniplaces-master/model/tensorflow/alexnet_train.py
+++ miniplaces-master/model/tensorflow/alexnet_train.py
@@ -53,16 +53,18 @@ def alexnet(x, keep_dropout, train_phase):
         'bo': tf.Variable(tf.zeros(100))
     }
 
-    # Conv + ReLU + LRN + Pool, 224->55->27
+    # Conv + BatchNorm + ReLU + Pool, 224->55->27
     conv1 = tf.nn.conv2d(x, weights['wc1'], strides=[1, 4, 4, 1], padding='SAME')
     conv1 = batch_norm_layer(conv1, train_phase,'bn1')
-    lrn1 = tf.nn.local_response_normalization(conv1, depth_radius=5, bias=1.0, alpha=1e-4, beta=0.75)
+    conv1 = tf.nn.relu(conv1)
+    # lrn1 = tf.nn.local_response_normalization(conv1, depth_radius=5, bias=1.0, alpha=1e-4, beta=0.75)
     pool1 = tf.nn.max_pool(lrn1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
 
     # Conv + ReLU + LRN + Pool, 27-> 13
     conv2 = tf.nn.conv2d(pool1, weights['wc2'], strides=[1, 1, 1, 1], padding='SAME')
     conv2 = batch_norm_layer(conv1, train_phase, 'bn2')
-    lrn2 = tf.nn.local_response_normalization(conv2, depth_radius=5, bias=1.0, alpha=1e-4, beta=0.75)
+    conv2 = tf.nn.relu(conv2)
+    # lrn2 = tf.nn.local_response_normalization(conv2, depth_radius=5, bias=1.0, alpha=1e-4, beta=0.75)
     pool2 = tf.nn.max_pool(lrn2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='SAME')
 
     # Conv + ReLU, 13-> 13
